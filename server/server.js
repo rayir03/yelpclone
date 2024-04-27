0bd9dc45c6da4c8b022ab3305f516a115cfedc89
rayir03 <rayir03@gmail.com>
2024-04-26 23:44:45 -0500

    all the routes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,66 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 
 
 
 const app  = express();
 
-app.get("/getRestaurants", (req, res) => {
-    
-})
+app.use(express.json());
+app.use(morgan("tiny"));
 
+//Get all restaurants
+
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ["Mcdonald", "KFC"],
+        }
+    });
+});
+
+//Get a restaurant
+
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ["Mcdonald", "KFC"],
+        }
+    });
+});
+
+//Create restaurant
+
+app.post("/api/v1/restaurants/:id", (req, res) => {
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: ["Mcdonald", "KFC"],
+        }
+    });
+});
+
+// Update restaurants
+
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ["Mcdonald", "KFC"],
+        }
+    });
+});
+
+//Delete
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success",
+    });
+});
 
 const port = process.env.PORT || 3008;
 app.listen(port, ()=> {
