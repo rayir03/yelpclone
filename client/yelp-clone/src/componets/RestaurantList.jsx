d01038c18f91b0d11bd5d587cff176b506ba67b2
rayir03 <rayir03@gmail.com>
2024-05-03 21:58:39 -0500

    delete finish

--- client/yelp-clone/src/componets/RestaurantList.jsx
+++ client/yelp-clone/src/componets/RestaurantList.jsx
@@ -20,6 +20,18 @@ const RestaurantList = (props) => {
             
         fetchData();
     },[]);
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await RestaurantFinder.delete(`/${id}`);
+            setRestaurants(restaurants.filter(restaurant => {
+                return restaurant.id !== id;
+            }))
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
     
   return (
     <div className="list-group">
@@ -44,7 +56,7 @@ const RestaurantList = (props) => {
                             <td>{"$".repeat(restaurant.price_range)}</td>
                             <td>reviews</td>
                             <td><button className="btn btn-warning">Update</button></td>
-                            <td><button className="btn btn-danger">Delete</button></td>
+                            <td><button onClick={() => handleDelete(restaurant.id)} className="btn btn-danger">Delete</button></td>
                         </tr>
                     )
                     
